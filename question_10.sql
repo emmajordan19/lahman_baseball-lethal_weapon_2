b8b2697c4fb9961459ee817fc648f66349b0cb29
nickforte <fortenw@gmail.com>
2022-02-17 11:20:18 -0600

    question_10 I am stumped

--- question_10.sql
+++ question_10.sql
@@ -2,13 +2,31 @@
 Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. 
 Report the players' first and last names and the number of home runs they hit in 2016. */
 
-SELECT DISTINCT people.playerid,
-       people.namefirst,
-	   people.namelast,
-       MAX(batting.hr) OVER(PARTITION BY batting.playerid, batting.yearid = 2016) AS max_hr,
-	   batting.yearid
-FROM people
-	 INNER JOIN batting
-	 ON batting.playerid = people.playerid
-WHERE batting.yearid = 2016
+
+SELECT DISTINCT playerid,
+	   yearid,
+       SUM(hr) OVER(PARTITION BY playerid, yearid) AS max_hr
+FROM batting
+GROUP BY playerid, yearid, hr
 ORDER BY max_hr DESC;
+
+----
+
+WITH query1 AS (SELECT playerid,
+	        	  		 yearid,
+             	  		 SUM(hr) OVER(PARTITION BY playerid, yearid) AS max_hr
+				  FROM batting
+	 			  GROUP BY playerid, yearid, hr
+	 			  ORDER BY max_hr DESC)
+SELECT DISTINCT playerid,
+	   MAX(max_hr),
+	   yearid				  
+FROM query1				  
+GROUP BY playerid, yearid
+ORDER BY MAX(max_hr) DESC;
+
+
+
+
+
+
